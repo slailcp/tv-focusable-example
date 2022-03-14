d74ee6e2c4391c30d956d0ea1e238b1c2497107b
shenlanlan <fjsll1102>
2022-03-14 10:15:27 +0800

    更新案例

--- react-tv-focusable-example/src/views/example2.js
+++ react-tv-focusable-example/src/views/example2.js
@@ -1,49 +1,36 @@
-import React, { Component } from "react";
-
-
+import React, { useState, useEffect } from "react";
 import '../common/public.css';
 
-// ie浏览器上是不可以使用getElementByPath获取el的哦
-class Example2 extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            active: 0,
-        }
-    }
-    componentDidMount() {
+export default function Example2(){
+    const [active,setActive] = useState(0)
+    useEffect(() => {
         window.$tv.offsetDistance = 50;
         window.$tv.requestFocus(window.$tv.getElementByPath('//div[@class="demo"]/span[9]'))
-    }
+    },[])
 
-
-    skip1() {
-        this.setState({ active: 1 })
+    const skip1 = () => {
+        setActive(1)
         window.$tv.requestFocus(window.$tv.getElementByPath('//div[@class="demo"]/span[55]'))
     }
 
-    skip2() {
-        this.setState({ active: 2 })
+    const skip2 = () => {
+        setActive(2)
         window.$tv.requestFocus(window.$tv.getElementByPath('//div[@class="demo"]/span[20]'), false);
     }
-    render() {
-        let lists = [];
-        for (let i = 0; i < 105; i++) {
-            lists.push(<span className="span" r-focusable="" ref={'fo' + (i + 1)} key={i}> {i + 1} </span>)
-        }
 
-        return (
-            <div className="example2-out">
-                ie浏览器上是不可以使用getElementByPath获取el的哦<br />
-                <span className={this.state.active === 1 ? 'btn active' : 'btn'} onClick={() => this.skip1()}>动效跳到第55个div</span> <br /> <br />
-                <span className={this.state.active === 2 ? 'btn active' : 'btn'} onClick={() => this.skip2()}>没有动效跳到第20个div</span>
-                <div className="demo">
-                    {lists}
-                </div>
-            </div>
-        )
+
+    let lists = [];
+    for (let i = 0; i < 105; i++) {
+        lists.push(<span className="span" r-focusable="" key={i}> {i + 1} </span>)
     }
+
+    return <div className="example2-out">
+            ie浏览器上是不可以使用getElementByPath获取el的哦<br />
+            <span className={active === 1 ? 'btn active' : 'btn'} onClick={() => skip1()}>动效跳到第55个div</span> <br /> <br />
+            <span className={active === 2 ? 'btn active' : 'btn'} onClick={() => skip2()}>没有动效跳到第20个div</span>
+            <div className="demo">
+                {lists}
+            </div>
+        </div>
+        
 }
-
-
-export default Example2;
